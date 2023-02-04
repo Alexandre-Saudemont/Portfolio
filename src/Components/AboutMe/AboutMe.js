1df04d69569624043f2ee57bdc0a86da7f8aab86
ElemenT <xetoxin@gmail.com>
2023-02-04 16:16:15 +0100

    ajout du formulaire de contact

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import "./AboutMe.css";
-
+import Contact from "../Contact/Contact.js"
 
 function AboutMe() {
     return (
         <section className='aboutme'>
-            <h1 className='aboutme-title'>Bonjour! Je m'appelle Alexandre Saudemont</h1>
+            <h1 className='aboutme-title'>Bonjour ! Je m'appelle Alexandre Saudemont</h1>
             <p className='aboutme-p'>
-                Je suis un développeur FullStack JavaScript.
-                J'ai étudié chez O'Clock du 17 Janvier jusqu'au 2 Juillet 2022 pour ma reconversion professionnelle
+                Après une formation de 6 mois pour devenir développeur FullStack JavaScript au sein de l'école O'Clock,
+                je suis actuellement à la recherche d'un premier emploi en tant que développeur junior.
             </p>
-
+            <Contact />
         </section>
     );
 }
--- src/router/Router.js
+++ src/router/Router.js
@@ -28,8 +28,7 @@ const router = createBrowserRouter([
                 <AboutMe />
 
             </>
-
-    }
+    },
 
 ])
 
