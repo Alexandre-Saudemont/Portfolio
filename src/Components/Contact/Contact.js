bbeaacf2e7cce3a5a57fa50f97c89c86155db0a7
ElemenT <xetoxin@gmail.com>
2023-02-09 18:31:57 +0100

    Finalisation de la page contact

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,8 +1,26 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import "./Contact.css";
+import chicken from "../../asset/img/chicken.gif"
 
 function Contact() {
+
+    // A l'envoie du formulaire je dois : toggle un state
+    // Avec le toggle, je fais un affichage conditionnel 
+    // DANS l'affichage conditionnel je fais mon composant modal
+    // dans mon composant modal, je dois mettre un button qui dois faire l'inverse du toggle pour fermer la modal
+    // au click de l'envoie du formulaire, je dois vider mon formulaire
+
+    const [toggleModalSuccess, setToggleModalSuccess] = useState(false);
+    const [toggleModalError, setToggleModalError] = useState(false);
+
+    function onClickToggleCloseModalError() {
+        setToggleModalError(!toggleModalError)
+    }
+
+    function onClickToggleCloseModalSuccess() {
+        setToggleModalSuccess(!toggleModalSuccess)
+    }
     const form = useRef();
 
     const sendEmail = (e) => {
@@ -10,8 +28,12 @@ function Contact() {
 
         emailjs.sendForm('contact_service', 'template_l2nwp6s', form.current, 'Z3rh0mNjILy5GvJ3-')
             .then((result) => {
+                setToggleModalSuccess(!toggleModalSuccess);
+                console.log("je clique sur le bouton envoyer", toggleModalSuccess)
                 console.log(result.text);
             }, (error) => {
+                setToggleModalError(!toggleModalError);
+                console.log("Je clique sur le bouton envoyer mais il y a une erreur", toggleModalError);
                 console.log(error.text);
             });
     };
@@ -19,11 +41,28 @@ function Contact() {
 
     return (
         <form ref={form} onSubmit={sendEmail} className="contact-form">
+            <img src={chicken} alt="chicken gif" />
             <p className='contact-p'>Me contacter :</p>
             <input className='contact-input' type="text" id="name" placeholder='Nom' />
             <input className='contact-input' type="email" id="email" placeholder='Exemple@gmail.com' />
             <textarea className='contact-input' name="message" rows="8" placeholder='Message' />
             <input type="submit" if="message" value="Envoyer" />
+            {toggleModalError &&
+                <div className='contact-container'>
+                    <div className='contact-container-modal'>
+                        <p className='contact-modal-error'>Une erreur est survenu, votre message n'à pas été envoyé.</p>
+                        <button type="button" className='contact-button' onClick={onClickToggleCloseModalError}>Fermer</button>
+                    </div>
+                </div>
+            }
+            {toggleModalSuccess &&
+                <div className='contact-container'>
+                    <div className='contact-container-modal'>
+                        <p className='contact-modal-success'>Votre message à bien été envoyé.</p>
+                        <button type="button" className='contact-button' onClick={onClickToggleCloseModalSuccess}>Fermer</button>
+                    </div>
+                </div>
+            }
         </form>
     );
 };
