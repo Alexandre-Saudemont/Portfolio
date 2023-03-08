ed709f96cb10c78e7a58f96dcbefa0f0724700e1
ElemenT <xetoxin@gmail.com>
2023-03-08 16:58:15 +0100

    ajout d'un projet et modification du CV en anglais

--- src/Components/CV/CV.js
+++ src/Components/CV/CV.js
@@ -1,30 +1,28 @@
 import './CV.css';
 import CValex from '../../asset/img/cv-alex.jpg';
 import {useOutletContext} from 'react-router-dom';
-
+import CValexEng from '../../asset/img/cv-alex-eng.jpg';
 function CV() {
 	const toggleTrad = useOutletContext();
 
 	return (
 		<div>
-			{/* <div>
-												<Document file={CVpdf} >		
-
-														<Page pageNumber={1} />
-												</Document>
-
-										</div> */}
 			<div className='cv-container'>
-				<img src={CValex} alt='CV alex' className='cv' />
 				<a href='https://drive.google.com/file/d/1ewYRavyeQoaVFf62dfIw5sRSWk4XhbIR/view?usp=share_link' target='_blank' rel='noreferrer'>
 					{toggleTrad ? (
-						<button type='submit' className='contact-button'>
-							Download CV
-						</button>
+						<div className='cv-img-container'>
+							<img src={CValexEng} alt='CV alex eng' className='cv' />
+							<button type='submit' className='contact-button'>
+								Download CV
+							</button>
+						</div>
 					) : (
-						<button type='submit' className='contact-button'>
-							Télécharger le CV
-						</button>
+						<div className='cv-img-container'>
+							<img src={CValex} alt='CV alex' className='cv' />
+							<button type='submit' className='contact-button'>
+								Télécharger le CV
+							</button>
+						</div>
 					)}
 				</a>
 			</div>
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,15 +1,68 @@
 import React from 'react';
 import {useOutletContext} from 'react-router-dom';
 import './Projects.css';
-
+import pokedeck from '../../asset/img/pokedeck.png';
 function Projects() {
 	const toggleTrad = useOutletContext();
+
 	return (
 		<div className='project-container'>
 			{toggleTrad ? (
-				<div className='project'>Work in Progress, come back later ! </div>
+				<>
+					<div className='project'>
+						Pokedeck website
+						<img src={pokedeck} alt='pokedeck' className='projet-pokedeck' />
+					</div>
+					<div className='project-button-container'>
+						<a
+							href='https://github.com/Alexandre-Saudemont/pokedeck-front'
+							type='submit'
+							target='_blank'
+							rel='noreferrer'
+							className='project-button'>
+							GitHub (frontend)
+						</a>
+						<a
+							href='https://github.com/Alexandre-Saudemont/pokedeck-back'
+							type='submit'
+							target='_blank'
+							rel='noreferrer'
+							className='project-button'>
+							GitHub (backend)
+						</a>
+					</div>
+					<a href='http://pokedeck.alexandre-saudemont.fr/' type='submit' target='_blank' rel='noreferrer' className='project-button'>
+						Demonstration
+					</a>
+				</>
 			) : (
-				<div className='project'>Projet en cours, repasser plus tard !</div>
+				<>
+					<div className='project'>
+						Site de Pokedeck
+						<img src={pokedeck} alt='pokedeck' className='projet-pokedeck' />
+					</div>
+					<div className='project-button-container'>
+						<a
+							href='https://github.com/Alexandre-Saudemont/pokedeck-front'
+							type='submit'
+							target='_blank'
+							rel='noreferrer'
+							className='project-button'>
+							GitHub (frontend)
+						</a>
+						<a
+							href='https://github.com/Alexandre-Saudemont/pokedeck-back'
+							type='submit'
+							target='_blank'
+							rel='noreferrer'
+							className='project-button'>
+							GitHub (backend)
+						</a>
+					</div>
+					<a href='http://pokedeck.alexandre-saudemont.fr/' type='submit' target='_blank' rel='noreferrer' className='project-button'>
+						Démonstration
+					</a>
+				</>
 			)}
 		</div>
 	);
